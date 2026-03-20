9ee2f10cbd7810f6ec3ee31f4112eaf02c5813c8
tmikada <29991871+tmikada@users.noreply.github.com>
2026-03-20 21:48:10 +0900

    パスワードバリデーション修正２

--- application/client/src/auth/validation.ts
+++ application/client/src/auth/validation.ts
@@ -13,7 +13,7 @@ export const validate = (values: AuthFormData): FormErrors<AuthFormData> => {
     errors.name = "名前を入力してください";
   }
 
-  if (normalizedPassword.length > 0 && normalizedPassword.length < 16 && /^[\p{Letter}\p{Number}]+$/u.test(normalizedPassword)) {
+  if (normalizedPassword.length > 0 && /^[\p{Letter}\p{Number}]+$/u.test(normalizedPassword)) {
     errors.password = "パスワードには記号を含める必要があります";
   }
   if (normalizedPassword.length === 0) {
